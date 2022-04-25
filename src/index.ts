ad4e4f45f5e3f611235c8488d5b11bb56826f647
Hamid Javadi <javadi.business@gmail.com>
2022-04-25 18:08:02 +0430

    - add editorconfig - add game setup inputs   - get connections count   - get game columns and rows git commit -m

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-console.clear();
-console.log('\x1b[36m', 'sometext', '\x1b[0m');
+import { Game } from "./types/game";
 
-const data = [
-    ['1', '2', '3', '4', '5'],
-    ['\x1b[43m\x1b[33mXX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m'],
-    ['\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m'],
-    ['\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m'],
-    ['\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m'],
-    ['\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m']
-];
-
-data.forEach((row) => {
-    console.log(row.concat().toString());
-})
+const game = new Game();
--- src/types/node.ts
+++ src/types/node.ts
@@ -1,19 +1,10 @@
 export type Node = {
-    col: number,
-    row: number,
-    status: NodeStatus
+  col: number,
+  row: number,
+  status: NodeStatus
 }
 
 export enum NodeStatus {
-    Empty = 0,
-    Filled = 1
-}
-
-export type Player = {
-    name: string,
-    color: Color
-}
-
-export enum Color {
-    Yellow = "#0ff"
+  Empty = 0,
+  Filled = 1
 }
