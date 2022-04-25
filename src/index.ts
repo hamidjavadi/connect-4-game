6c094c34d13fc34ab12c2cdaf0e1d2bc27db1a7f
Hamid Javadi <javadi.business@gmail.com>
2022-04-25 14:02:10 +0430

    add node types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('start...')
+console.clear();
+console.log('\x1b[36m', 'sometext', '\x1b[0m');
+
+const data = [
+    ['1', '2', '3', '4', '5'],
+    ['\x1b[43m\x1b[33mXX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m'],
+    ['\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m'],
+    ['\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m'],
+    ['\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m'],
+    ['\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m', '\x1b[33mX\x1b[0m']
+];
+
+data.forEach((row) => {
+    console.log(row.concat().toString());
+})
